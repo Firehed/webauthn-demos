526688d73f96d3d9b0d3cea7bc6ae343a2932848
Eric Stern <eric@ericstern.com>
2022-11-09 14:37:21 -0800

    Allow env vars to work in FE Docker image (#1)
    Add another startup script (auto-detected by the nginx image) to perform
    `envsubst` on the contents of `index.html`. Squash a bunch of stuff
    together in the env reading file to combine the sources for the static
    prod build and the local implementation.

--- react/src/env.ts
+++ react/src/env.ts
@@ -1,1 +1,27 @@
-export const API_HOST = process.env.REACT_APP_API_HOST
+interface EnvVars {
+  [key: string]: string
+}
+
+// @ts-ignore this value is set in public/index.html
+const INJECTED_ENV_DATA: EnvVars = window.ENV || {}
+const BUILDTIME_ENV_DATA = process.env || {}
+
+// Attempt to follow the same naming conventions and restrictions imposed by
+// CRA (i.e. the two magic values + anything starting with REACT_APP). This
+// won't stop you from setting inappriate values, but it slims the surface area
+const SANITIZED_INJECTED_ENV_DATA = Object.keys(INJECTED_ENV_DATA)
+  .filter(key => key === 'NODE_ENV' || key === 'PUBLIC_URL' || key.startsWith('REACT_APP_'))
+  .reduce((obj, key) => {
+    const val = INJECTED_ENV_DATA[key]
+    // Remove anything that didn't get substituted or is blank
+    if (val !== '$' + key && val !== '') {
+      obj[key] = val
+    }
+    return obj
+  }, {} as EnvVars)
+
+const combined = { ...BUILDTIME_ENV_DATA, ...SANITIZED_INJECTED_ENV_DATA }
+
+// Actual config follows
+
+export const API_HOST = combined.REACT_APP_API_HOST
